bfadc5673cc168a676905e264cf9922248086386
rhesaav <rhesaav@gmail.com>
2019-09-29 15:23:38 +0700

    - add fragment - create fragment activity and java class

---
+++ app/src/main/java/id/transdigital/fileservicemanagement/HistoryFragment.java
@@ -0,0 +1,18 @@
+package id.transdigital.fileservicemanagement;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HistoryFragment extends Fragment {
+    @Nullable
+    @Override
+
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragmenthistory, container, false);
+        return view;
+    }
+}
