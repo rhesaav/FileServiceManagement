d9a3cc04488f7c7d867d219bd55c18daab82785e
rhesaav <rhesaav@gmail.com>
2019-09-30 16:12:30 +0700

    - custom layout absensi

--- app/src/main/java/id/transdigital/fileservicemanagement/AbesensiActivity.java
+++ app/src/main/java/id/transdigital/fileservicemanagement/AbesensiActivity.java
@@ -1,7 +1,10 @@
 package id.transdigital.fileservicemanagement;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class AbesensiActivity extends AppCompatActivity {
 
@@ -9,5 +12,28 @@ public class AbesensiActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_abesensi);
+
+        initViews();
+    }
+
+    private void initViews() {
+        // setting toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //seting viewpager
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        setupViewPager(viewPager);
+
+        // setting tabLayout
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        MainFragmentPagerAdapter mainFragmentPagerAdapter = new MainFragmentPagerAdapter(getSupportFragmentManager());
+        mainFragmentPagerAdapter.addFragment(new DayFragment(), getString(R.string.day));
+        mainFragmentPagerAdapter.addFragment(new HistoryFragment(), getString(R.string.history));
+        viewPager.setAdapter(mainFragmentPagerAdapter);
     }
 }
--- app/src/main/java/id/transdigital/fileservicemanagement/MainActivity.java
+++ app/src/main/java/id/transdigital/fileservicemanagement/MainActivity.java
@@ -2,36 +2,29 @@ package id.transdigital.fileservicemanagement;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-
-import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     final String url = "https://api.tadabase.io/api/v1/data-tables";
     RequestQueue queue = Volley.newRequestQueue(this);
     //prepare the request
-    JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, url, null,
-            new Response.Listener<JSONObject>() {
-                @Override
-                public void onResponse(JSONObject response) {
-                    Log.d("Response", response.toString());
-                }
-            },
-            new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Log.d("Error.Response", response);
-                }
-            }
-    );
+//    JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+//            new Response.Listener<JSONObject>() {
+//                @Override
+//                public void onResponse(JSONObject response) {
+//                    Log.d("Response", response.toString());
+//                }
+//            },
+//            new Response.ErrorListener() {
+//                @Override
+//                public void onErrorResponse(VolleyError error) {
+//                    Log.d("Error.Response", response);
+//                }
+//            }
+//    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,5 +33,5 @@ public class MainActivity extends AppCompatActivity {
 
     }
     //to request queue
-    queue.add(getRequest)
+//    queue.add(getRequest)
 }
